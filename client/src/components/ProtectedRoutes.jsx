79804e706d9c499848dd40df33f113a4e1b517bd
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-05-09 17:10:20 +0530

    Add ProtectedRoutes component to enforce authentication for protected routes

---
+++ client/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default function ProtectedRoutes({children}) {
+    const { user } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    },[])
+  return (
+    <div>{children}</div>
+  )
+}
