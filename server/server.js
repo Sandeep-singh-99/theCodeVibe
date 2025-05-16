72559899c0d516637cddcce58772386df2970290
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-05-16 17:40:39 +0530

    feat: Update CORS origin to use production URL for server and socket

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ import { app, server } from './socket/socket.js'
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "https://thecodevibe-1.onrender.com",
     credentials: true,
 }));
 app.use(cookieParser());
